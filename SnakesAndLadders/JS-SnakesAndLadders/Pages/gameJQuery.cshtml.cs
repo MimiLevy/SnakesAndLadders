25238e2e464127c59b70e32564b98b7a3b485c9e
Mimi Levy <264mimi@gmail.com>
2024-03-28 11:17:17 +0200

    P3 S11 HW #89

---
+++ SnakesAndLadders/JS-SnakesAndLadders/Pages/gameJQuery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JS_SnakesAndLadders.Pages
+{
+    public class gameJQueryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
